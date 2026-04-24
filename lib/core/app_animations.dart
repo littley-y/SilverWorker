fda111e111c5c85a70e457364927f10ac41d68eb
Yeongtae Hyun <hyunyoungtae@gmail.com>
2026-04-24 15:07:37 +0900

    chore(init): bootstrap devops worktree (structure only)

--- lib/core/app_animations.dart
+++
@@ -1,5 +0,0 @@
-class AppAnimations {
-  static const Duration fast = Duration(milliseconds: 300);
-  static const Duration normal = Duration(milliseconds: 500);
-  static const Duration slow = Duration(milliseconds: 1000);
-}
